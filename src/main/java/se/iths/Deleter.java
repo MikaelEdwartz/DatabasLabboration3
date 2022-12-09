b165efe9635231adac11ce1b577fe28cec6374e7
mikaeledwartz <Mikael.edwartz@gmail.com>
2022-12-09 09:32:18 +0100

    add Deleter class with functionality to delete from database

--- src/main/java/se/iths/Deleter.java
+++ src/main/java/se/iths/Deleter.java
@@ -1,5 +1,7 @@
 package se.iths;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Deleter {
@@ -12,12 +14,37 @@ public class Deleter {
                         "|1. Radera från spel      |\n" +
                         "|2. Radera från kategorier|\n" +
                         "===========================");
+
+        switch(choice){
+            case 1 -> deleteFromGames(getIntInput("Vad är id't på spelet du vill radera?"));
+            case 2 -> deleteFromCategories(getIntInput("Vad är id't på kategorin du vill radera?"));
+        }
     }
 
+    private static void deleteFromGames(int spelID) {
+        var sql = "DELETE FROM spel WHERE spelID = ?";
 
+        try(PreparedStatement preparedStatement = Connector.connect().prepareStatement(sql)){
 
+            preparedStatement.setInt(1, spelID);
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private static void deleteFromCategories(int kategoriID) {
+        var sql = "DELETE FROM kategori WHERE kategoriID = ?";
+
+        try(PreparedStatement preparedStatement = Connector.connect().prepareStatement(sql)){
+
+            preparedStatement.setInt(1, kategoriID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private static int getIntInput(String prompt) {
         System.out.println(prompt);
